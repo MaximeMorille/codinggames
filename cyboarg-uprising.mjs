bbe0f54a155c33ed602625b080bb592f86866710
Maxime Morille <maxime.morille@braincube.com>
2022-11-28 23:12:02 +0100

    82e

--- cyboarg-uprising.mjs
+++ cyboarg-uprising.mjs
@@ -2,244 +2,295 @@
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-
-const factories = {}
-const OWNERSHIP = ['ENNEMY', 'NEUTRAL', 'ME']
-let bombs = 2
-const bombed = []
-let turn = 0
-const NB_ATTACKING_BASES = 3
+const FACTORIES = {};
+const OWNERSHIP = ['ENNEMY', 'NEUTRAL', 'ME'];
+let bombs = 2;
+const bombed = [];
+let turn = 0;
+const NB_ATTACKING_BASES = 3;
 
 function initIfNeededFactory(factory) {
-    if (!factories[factory]) {
-        factories[factory] = { id: factory, links: {} }
+    if (!FACTORIES[factory]) {
+        FACTORIES[factory] = { id: factory, links: {} };
     }
 }
 
-function updateFactoryState(factory, state) {
-    // console.error(`Current factory ${factory}`, factories[factory]);
-    // console.error('State to change', state);
-    factories[factory] = { ...factories[factory], ...state }
-    // console.error(`Future factory ${factory}`, factories[factory]);
+function updateFactoryState(factoryId, state, debug = false) {
+    debug && console.error(`Current factory ${factoryId}`, FACTORIES[factoryId]);
+    debug && console.error('State to change', state);
+    FACTORIES[factoryId] = { ...FACTORIES[factoryId], ...state };
+    debug && console.error(`Future factory ${factoryId}`, FACTORIES[factoryId]);
 }
 
 function addsLink(factory1, factory2, distance) {
-    initIfNeededFactory(factory1)
-    initIfNeededFactory(factory2)
+    initIfNeededFactory(factory1);
+    initIfNeededFactory(factory2);
 
     updateFactoryState(factory1, {
-        links: { ...factories[factory1].links, ...{ [factory2]: distance } },
-    })
+        links: { ...FACTORIES[factory1].links, ...{ [factory2]: distance } },
+    });
     updateFactoryState(factory2, {
-        links: { ...factories[factory2].links, ...{ [factory1]: distance } },
-    })
+        links: { ...FACTORIES[factory2].links, ...{ [factory1]: distance } },
+    });
+}
+
+function withTroops(factory) {
+    return factory.troops > 0;
 }
 
 function withAtLeastFourTroops(factory) {
-    return factory.troops > 4
+    return factory.troops > 4;
 }
 
 function withAtLeastTenTroops(factory) {
-    return factory.troops > 10
+    return factory.troops > 10;
 }
 
 function withLessThanTenTroops(factory) {
-    return factory.troops < 10
+    return factory.troops < 10;
+}
+
+function withProduction(factory) {
+    return factory.production > 0;
 }
 
 function withNotFullProduction(factory) {
-    return factory.production < 3
+    return factory.production < 3;
 }
 
 function sortByDecreasingTroops(f1, f2) {
-    return f2.troops - f1.troops
+    return f2.troops - f1.troops;
 }
 
-export function setTargetedHostileFactory(factory, notOwnedFactories) {
-    const { factory: nearestHostile, distance: distanceToNearestHostile } =
-        notOwnedFactories.reduce(
-            (result, hostileFactory) => {
-                const isProductive =
-                    result.production < hostileFactory.production
-                const isNearer =
-                    result.distance > hostileFactory.links[factory.id]
+function setNearestAllyFactory(factory, allyFactories) {
+    const { factory: nearestAlly, distance: distanceToNearestAlly } = searchMostProductiveAmongNearestFactory(
+        factory,
+        allyFactories
+    );
 
-                if (isNearer && isProductive) {
-                    return {
-                        factory: hostileFactory,
-                        distance: hostileFactory.links[factory.id],
-                        production: hostileFactory.production,
-                    }
-                }
+    updateFactoryState(factory.id, { nearestAlly, distanceToNearestAlly });
 
-                if (isNearer) {
-                    return {
-                        factory: hostileFactory,
-                        distance: hostileFactory.links[factory.id],
-                        production: hostileFactory.production,
-                    }
-                }
+    return FACTORIES[factory.id];
+}
 
-                return result
-            },
-            {
-                factory: undefined,
-                distance: Number.POSITIVE_INFINITY,
-                production: 0,
+function setNeutralFactories(factory, factories) {
+    function predicate(f1, f2) {
+        const f1Distance = factory.links[f1.id];
+        const f2Distance = factory.links[f2.id];
+
+        if (f1Distance === f2Distance) {
+            return f1.production - f2.production;
+        }
+
+        return f1Distance - f2Distance;
+    }
+
+    const sortedFactories = factories.sort(predicate);
+    const { factory: nearestNeutral, distance: distanceToNearestNeutral } = searchMostProductiveAmongNearestFactory(
+        factory,
+        factories
+    );
+
+    updateFactoryState(factory.id, { nearestNeutral, distanceToNearestNeutral, neutrals: sortedFactories });
+
+    return FACTORIES[factory.id];
+}
+
+function setNearestHostileFactory(factory, factories) {
+    const { factory: nearestHostile, distance: distanceToNearestHostile } = searchMostProductiveAmongNearestFactory(
+        factory,
+        factories
+    );
+
+    updateFactoryState(factory.id, { nearestHostile, distanceToNearestHostile });
+
+    return FACTORIES[factory.id];
+}
+
+function searchMostProductiveAmongNearestFactory(factory, factories) {
+    return factories.reduce(
+        (result, hostileFactory) => {
+            const isProductive = result.production < hostileFactory.production;
+            const isNearer = result.distance > hostileFactory.links[factory.id];
+
+            if (isNearer && isProductive) {
+                return {
+                    factory: hostileFactory,
+                    distance: hostileFactory.links[factory.id],
+                    production: hostileFactory.production,
+                };
             }
-        )
-    updateFactoryState(factory.id, { nearestHostile, distanceToNearestHostile })
 
-    return factories[factory.id]
+            if (isNearer) {
+                return {
+                    factory: hostileFactory,
+                    distance: hostileFactory.links[factory.id],
+                    production: hostileFactory.production,
+                };
+            }
+
+            return result;
+        },
+        {
+            factory: undefined,
+            distance: Number.POSITIVE_INFINITY,
+            production: 0,
+        }
+    );
 }
 
 function sortByNearestHostile(f1, f2) {
-    return f1.distanceToNearestHostile - f2.distanceToNearestHostile
+    return f1.distanceToNearestHostile - f2.distanceToNearestHostile;
 }
 
-function moveTroopsToNeedingBase(factory, toReinforce) {
-    const candidate = toReinforce.reduce(
-        (result, f) => {
-            const currentDistance = f.links[factory.id]
-            const isNearer = result && result.distance > currentDistance
-
-            if (isNearer) {
-                return { ...f, distance: currentDistance }
-            }
-
-            return result
-        },
-        { distance: Number.POSITIVE_INFINITY }
-    )
-
-    return `MOVE ${factory.id} ${candidate.id} 3;`
+function moveTroopsToNeedingBase(factory) {
+    const movingTroops = 3;
+    if (!factory.nearestAlly) {
+        return `MSG missing ally for ${factory.id};`;
+    }
+    updateFactoryState(factory.id, { troops: factory.troops - movingTroops });
+    return `MOVE ${factory.id} ${factory.nearestAlly.id} ${movingTroops};`;
 }
 
-function enactBombCommand(notMine, myFactories, turn) {
-    const ennemiesWithProd3 = notMine.filter(
-        (f) => f.owner === 'ENNEMY' && f.production === 3
-    )
+function buildBombCommand(notMine, myFactories, turn) {
+    const targets = notMine.filter((f) => (f.owner === 'ENNEMY' && f.production === 3) || turn === 0);
 
-    let bombCmd = ''
-    if (myFactories.length > 0 && turn > 5) {
-        for (let i = 0; i < ennemiesWithProd3.length; i++) {
-            const toBomb = ennemiesWithProd3[i]
+    let bombCmd = '';
+    if (myFactories.length > 0) {
+        for (let i = 0; i < targets.length; i++) {
+            const toBomb = targets[i];
             if (bombs > 0 && !bombed.includes(toBomb.id)) {
-                bombCmd += `BOMB ${myFactories.at(0).id} ${toBomb.id};`
-                bombed.push(toBomb.id)
-                bombs--
+                bombCmd += `BOMB ${myFactories.at(0).id} ${toBomb.id};`;
+                bombed.push(toBomb.id);
+                bombs--;
             }
         }
     }
-    return bombCmd
+    return bombCmd;
+}
+
+function buildIncreaseCommand(myFactories) {
+    let increaseCmd = '';
+    increaseCmd = myFactories
+        .filter(withAtLeastTenTroops)
+        .map((f) => {
+            if (f.production === 3) {
+                return moveTroopsToNeedingBase(f);
+            }
+            return `INC ${f.id};`;
+        })
+        .join('');
+    return increaseCmd;
+}
+
+function buildAttackCommand(myFactories) {
+    if (myFactories.length === 0) {
+        return 'WAIT';
+    }
+
+    return myFactories.reduce((actualCommand, factory) => {
+        let newCommand = '';
+        let currentTroops = factory.troops;
+        factory.neutrals.forEach((f) => {
+            if (currentTroops > 0) {
+                const force = f.troops + 1;
+                newCommand += `MOVE ${factory.id} ${f.id} ${force};`;
+                currentTroops -= force;
+            }
+        });
+
+        if (currentTroops > 0 && factory.nearestHostile !== undefined) {
+            newCommand += `MOVE ${factory.id} ${factory.nearestHostile.id} ${factory.troops};`;
+        }
+
+        if (newCommand === '') {
+            newCommand = 'WAIT';
+        }
+
+        if (actualCommand === '') {
+            return newCommand;
+        }
+
+        return `${actualCommand}; ${newCommand}`;
+    }, '');
+}
+
+function cleanCommand(cmd) {
+    let str = cmd.replaceAll(';;', ';');
+    if (str.endsWith(';')) {
+        return (str += 'WAIT');
+    }
+    return str;
 }
 
 function buildOrders(turn) {
-    let [myFactories, notMine] = Object.keys(factories).reduce(
-        ([tmpOwned, tmpNotOwned], id) => {
-            if (factories[id].owner === 'ME') {
-                return [[...tmpOwned, factories[id]], tmpNotOwned]
+    let [myFactories, neutralFactories, ennemyFactories] = Object.keys(FACTORIES).reduce(
+        ([tmpOwned, tmpNeutral, tmpNotOwned], id) => {
+            if (FACTORIES[id].owner === 'ME') {
+                return [[...tmpOwned, FACTORIES[id]], tmpNeutral, tmpNotOwned];
             }
-            return [tmpOwned, [...tmpNotOwned, factories[id]]]
+            if (FACTORIES[id].owner === 'NEUTRAL') {
+                return [tmpOwned, [...tmpNeutral, FACTORIES[id]], tmpNotOwned];
+            }
+            return [tmpOwned, tmpNeutral, [...tmpNotOwned, FACTORIES[id]]];
         },
-        [[], []]
-    )
+        [[], [], []]
+    );
 
-    let bombCmd = enactBombCommand(notMine, myFactories, turn)
+    myFactories = myFactories.map((factory) => {
+        let enhancedFactory = setNearestAllyFactory(factory, myFactories.filter(withLessThanTenTroops));
+        const neutralsWithProduction = neutralFactories.filter(withProduction);
+        enhancedFactory = setNeutralFactories(enhancedFactory, neutralsWithProduction);
+        enhancedFactory = setNearestHostileFactory(enhancedFactory, ennemyFactories);
+        return enhancedFactory;
+    });
 
-    myFactories = myFactories.map((factory) =>
-        setTargetedHostileFactory(factory, notMine)
-    )
+    const bombCmd = buildBombCommand(ennemyFactories, myFactories, turn);
+    const attackCmd = buildAttackCommand(myFactories.filter(withAtLeastFourTroops));
+    const increaseCmd = buildIncreaseCommand(myFactories);
 
-    const sortedFactories = myFactories
-        .filter(withAtLeastFourTroops)
-        .sort(sortByNearestHostile)
-    const factorieswithAtLeastFourTroops = sortedFactories.slice(
-        0,
-        NB_ATTACKING_BASES
-    )
-
-    if (factorieswithAtLeastFourTroops.length === 0) {
-        return 'WAIT'
-    }
-
-    let increaseCmd = ''
-    if (sortedFactories.length > NB_ATTACKING_BASES) {
-        const toUpgrade = sortedFactories.slice(NB_ATTACKING_BASES)
-        const toReinforce = myFactories
-            .filter(withNotFullProduction)
-            .filter(withLessThanTenTroops)
-        increaseCmd = toUpgrade
-            .filter(withAtLeastTenTroops)
-            .map((f) => {
-                if (f.production === 3) {
-                    return moveTroopsToNeedingBase(f, toReinforce)
-                }
-                return `INC ${f.id};`
-            })
-            .join('')
-    }
-
-    return (
-        bombCmd +
-        increaseCmd +
-        factorieswithAtLeastFourTroops.reduce((actualCommand, factory) => {
-            let newCommand = 'WAIT'
-
-            if (factory.nearestHostile !== undefined) {
-                newCommand = `MOVE ${factory.id} ${factory.nearestHostile.id} ${
-                    factory.troops - 3
-                }`
-            }
-
-            if (actualCommand === '') {
-                return newCommand
-            }
-
-            return `${actualCommand}; ${newCommand}`
-        }, '')
-    )
+    return cleanCommand(bombCmd + increaseCmd + attackCmd);
 }
 
-const factoryCount = parseInt(readline()) // the number of factories
-const linkCount = parseInt(readline()) // the number of links between factories
+const factoryCount = parseInt(readline()); // the number of factories
+const linkCount = parseInt(readline()); // the number of links between factories
 for (let i = 0; i < linkCount; i++) {
-    var inputs = readline().split(' ')
-    const factory1 = parseInt(inputs[0])
-    const factory2 = parseInt(inputs[1])
-    const distance = parseInt(inputs[2])
+    var inputs = readline().split(' ');
+    const factory1 = parseInt(inputs[0]);
+    const factory2 = parseInt(inputs[1]);
+    const distance = parseInt(inputs[2]);
 
-    addsLink(factory1, factory2, distance)
+    addsLink(factory1, factory2, distance);
 }
 
-console.error('START GAME LOOP!!!')
+console.error('START GAME LOOP!!!');
 
 // game loop
 while (true) {
-    const entityCount = parseInt(readline()) // the number of entities (e.g. factories and troops)
+    const entityCount = parseInt(readline()); // the number of entities (e.g. factories and troops)
     for (let i = 0; i < entityCount; i++) {
-        var inputs = readline().split(' ')
-        const entityId = parseInt(inputs[0])
-        const entityType = inputs[1]
-        const arg1 = parseInt(inputs[2])
-        const arg2 = parseInt(inputs[3])
-        const arg3 = parseInt(inputs[4])
-        const arg4 = parseInt(inputs[5])
-        const arg5 = parseInt(inputs[6])
+        var inputs = readline().split(' ');
+        const entityId = parseInt(inputs[0]);
+        const entityType = inputs[1];
+        const arg1 = parseInt(inputs[2]);
+        const arg2 = parseInt(inputs[3]);
+        const arg3 = parseInt(inputs[4]);
+        const arg4 = parseInt(inputs[5]);
+        const arg5 = parseInt(inputs[6]);
 
         if (entityType === 'FACTORY') {
-            const owner = OWNERSHIP[arg1 + 1]
+            const owner = OWNERSHIP[arg1 + 1];
             updateFactoryState(entityId, {
                 owner,
                 troops: arg2,
                 production: arg3,
-            })
+            });
         }
     }
 
-    const orders = buildOrders(turn)
+    const orders = buildOrders(turn);
 
     // Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
-    console.log(orders)
-    turn++
+    console.log(orders);
+    turn++;
 }
